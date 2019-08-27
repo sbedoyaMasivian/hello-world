472a99f84a419c1296d9a69d17154d54575c5a60
sbedoyaMasivian <santiago.bedoya@masivian.com>
2019-08-27 16:34:53 -0500

    first commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="onReady"></ckeditor>
+	</div>
+</template>
+
+<script>
+	import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+	
+	export default {
+		name: 'app',
+		data() {
+			return {
+				editor: DecoupledEditor,
+				editorData: '<p>Content of the editor.</p>',
+				editorConfig: {
+				}
+			};
+		},
+		methods: {
+			onReady( editor )  {
+                document.body.appendChild( editor.ui.view.toolbar.element );
+                document.body.appendChild( editor.ui.view.editable.element );
+			}
+		}
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
+Vue.use( CKEditor );
 Vue.config.productionTip = false
 
 new Vue({
