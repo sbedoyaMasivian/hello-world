a77b8ffc047de34bc312307d1fbc8a62182eaf8c
sbedoyaMasivian <santiago.bedoya@masivian.com>
2019-08-27 17:43:51 -0500

    Encapsulamiento del editor en un div

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,6 +1,8 @@
-<template>
+<!--<template>
 	<div id="app">
-		<ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="onReady"></ckeditor>
+        <div id="wraper">
+            <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="onReady"></ckeditor>
+        </div>
 	</div>
 </template>
 
@@ -19,9 +21,42 @@
 		},
 		methods: {
 			onReady( editor )  {
-                document.body.appendChild( editor.ui.view.toolbar.element );
-                document.body.appendChild( editor.ui.view.editable.element );
+                document.getElementById("app").appendChild( editor.ui.view.toolbar.element );
+                document.getElementById("app").appendChild( editor.ui.view.editable.element );
 			}
 		}
 	}
+</script>-->
+<template>
+    <div id="app">
+        <div class="document-editor">
+            <div id="toolbar" class="document-editor__toolbar"></div>
+            <div class="document-editor__editable-container">
+            <div class="document-editor__editable">
+                <ckeditor toolbar-container="#toolbar" :editor="editor" v-model="editorData" @ready="onReady" ></ckeditor>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<script>
+    import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+    
+    export default {
+        name: 'app',
+        data() {
+            return {
+                editor: DecoupledEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                }
+            };
+        },
+        methods: {
+            onReady( editor ) {
+                document.getElementById("toolbar").appendChild( editor.ui.view.toolbar.element );
+            }
+        }
+    }
 </script>
